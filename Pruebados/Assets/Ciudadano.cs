c70092e76fe6fc16234b6405e60f30d2cbe1a37b
cheitovilla <>
2018-05-28 19:19:44 -0500

    Finalizado 015 Enums y Struct
    Ready

---
+++ Pruebados/Assets/Ciudadano.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ciudadano : MonoBehaviour {
+
+    public GameObject ciudadanoMesh;
+    int age;
+    NameCiudadano nameCiudadano;
+    // Use this for initialization
+    void Start() {
+        ciudadanoMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        nameCiudadano = (NameCiudadano)Random.Range(0, (int)NameCiudadano.Lenght);
+        age = Random.Range(15, 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public CiudadanoInfo GetInfo()
+    {
+        CiudadanoInfo info = new CiudadanoInfo()
+        {
+            age = age,
+            nameCiudadano = nameCiudadano
+        };
+        return info;
+    }
+
+}
---
+++ Pruebados/Assets/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorZombie {
+    magenta,cyan, green, Lenght 
+}
+
+
+public enum BodyParts
+{
+    Cabeza, Brazo, Pierna, Culo, Pito, Lenght
+}
+
+public enum NameCiudadano
+{
+    Juan, Pedro, Mateo, Marcos, Judas, Teo, Wardijestrudis, Barrabas, Willian, Elkin, Abrahan, Santiago,
+    Pablo, Cohelo, Daniela, Ramirez, Salomon, Sanson, Aquiles, Goliat, Lenght
+}
+
+
+public struct ZombieInfo
+{
+    public Color colorZombie;
+    public BodyParts favoritePart;
+}
+
+public struct CiudadanoInfo {
+    public int age;
+    public NameCiudadano nameCiudadano;
+}
+
+
